cccc055bf60cab3aa7ea3d4e38d2325efeb70542
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-08 23:25:47 +0300

    feat: init

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use config::{Config, ConfigError, Environment};
+use dotenv::dotenv;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ConnectorConfig {
+    pub log: LogConfig,
+    pub collections: Vec<CollectionConfig>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct LogConfig {
+    pub level: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CollectionConfig {
+    pub db_name: String,
+    pub coll_name: String,
+    pub change_stream_pre_and_post_images: bool,
+    pub tokens_db_name: String,
+    pub tokens_coll_name: String,
+    pub tokens_coll_capped: Option<bool>,
+    pub tokens_coll_size_in_bytes: Option<u64>,
+    pub stream_name: String,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct Settings {
+    pub connector: ConnectorConfig,
+}
+
+impl Settings {
+    pub fn from_env() -> Result<Self, ConfigError> {
+        dotenv().ok();
+        let mut cfg = Config::builder();
+        cfg = cfg.add_source(Environment::default().separator("__"));
+        cfg.build()?.try_deserialize()
+    }
+}
