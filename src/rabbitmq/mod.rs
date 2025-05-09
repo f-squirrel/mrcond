7aebe757db157da7f3effd9ec145a3c90a222498
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-09 20:29:23 +0300

    chore: make wrapper for trait

--- src/rabbitmq/mod.rs
+++ src/rabbitmq/mod.rs
@@ -1,5 +1,22 @@
 pub mod amqp;
 pub mod error;
 pub mod publish;
-pub use amqp::Publisher;
 pub use error::Error;
+use mongodb::{bson::Document, change_stream::event::ChangeStreamEvent};
+
+use crate::rabbitmq::publish::Publish;
+use std::sync::Arc;
+
+pub struct Publisher {
+    inner: Arc<dyn Publish>,
+}
+
+impl Publisher {
+    pub fn new(inner: Arc<dyn Publish>) -> Self {
+        Self { inner }
+    }
+
+    pub async fn publish(&self, event: &ChangeStreamEvent<Document>) -> Result<(), Error> {
+        self.inner.publish(event).await
+    }
+}
