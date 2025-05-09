c6e0d5d88692623625c5543fc305c441f4d20d39
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-09 13:49:19 +0300

    feat: save resume token

--- src/mongo/watcher.rs
+++ src/mongo/watcher.rs
@@ -9,7 +9,6 @@ pub struct Watcher {
     pub db_name: String,
     pub coll_name: String,
     pub change_stream_pre_and_post_images: bool,
-    pub resume_token: Option<ResumeToken>,
     pub resume_tokens: ResumeTokens,
 }
 
@@ -20,49 +19,49 @@ impl Watcher {
         coll_name: String,
         change_stream_pre_and_post_images: bool,
         resume_tokens: ResumeTokens,
-        stream_name: &str,
     ) -> mongodb::error::Result<Self> {
-        // Try to get the last resume token from storage
-        let token_bson = resume_tokens.get_last_resume_token(stream_name).await?;
-        let resume_token = token_bson.and_then(|b| bson::from_bson(b).ok());
         Ok(Self {
             client,
             db_name,
             coll_name,
             change_stream_pre_and_post_images,
-            resume_token,
             resume_tokens,
         })
     }
 
-    pub async fn watch(&mut self, stream_name: &str) -> mongodb::error::Result<()> {
+    pub async fn watch(&self, stream_name: &str) -> mongodb::error::Result<()> {
         let collection = self
             .client
             .database(&self.db_name)
             .collection::<Document>(&self.coll_name);
-        let mut change_stream = collection
-            .watch()
-            .resume_after(self.resume_token.clone())
-            .await?;
+
+        let resume_token = self
+            .resume_tokens
+            .get_last_resume_token(stream_name)
+            .await?
+            .and_then(|b| bson::from_bson(b).ok());
+
+        let mut change_stream = collection.watch().resume_after(resume_token).await?;
         info!(db = %self.db_name, coll = %self.coll_name, "Started watching collection");
         while let Some(event) = change_stream.next().await {
             match event {
                 Ok(change) => {
-                    let resume_token = change_stream.resume_token();
                     debug!(?change, "Change event");
-                    // Save the new resume token
-                    if let Some(token) = resume_token {
+
+                    if let Some(token) = change_stream.resume_token() {
                         if let Err(e) = self
                             .resume_tokens
-                            .set_last_resume_token(stream_name, bson::to_bson(&token).unwrap())
+                            .set_last_resume_token(stream_name, bson::to_bson(&token)?)
                             .await
                         {
                             error!(error = %e, "Failed to save resume token");
+                            break;
                         }
                     }
                 }
                 Err(e) => {
                     error!(error = %e, "Change stream error");
+                    break;
                 }
             }
         }
