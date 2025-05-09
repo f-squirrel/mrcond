5e38ddb8f7a00854a26c66ebf43ab854918aec78
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-09 11:40:17 +0300

    refactor: split config

--- src/config.rs
+++ src/config.rs
@@ -3,19 +3,36 @@ use dotenv::dotenv;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone)]
-pub struct Collection {
+pub struct WatchedDb {
     pub db_name: String,
     pub coll_name: String,
     pub change_stream_pre_and_post_images: bool,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct ResumeTokensDB {
     pub tokens_db_name: String,
     pub tokens_coll_name: String,
     pub tokens_coll_capped: Option<bool>,
     pub tokens_coll_size_in_bytes: Option<u64>,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct RabbitMq {
     pub stream_name: String,
 }
 
 #[derive(Debug, Deserialize, Clone)]
+pub struct Collection {
+    pub watched: WatchedDb,
+    pub resume_tokens: ResumeTokensDB,
+    pub rabbitmq: RabbitMq,
+}
+
+#[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
+    pub mongo_uri: String,
+    pub rabbitmq_uri: String,
     pub collections: Vec<Collection>,
 }
 
