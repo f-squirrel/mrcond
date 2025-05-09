4c723dfbee463af4dc47d0ec76f8cacb6173ec00
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-10 00:51:54 +0300

    refactor: extract spawn function

--- mongodb_rabbitmq_connector/src/server.rs
+++ mongodb_rabbitmq_connector/src/server.rs
@@ -23,6 +23,37 @@ impl ConnectorServer {
         Self { settings }
     }
 
+    fn spawn(
+        settings: Settings,
+        collection: crate::config::Collection,
+        tx: tokio::sync::mpsc::UnboundedSender<crate::config::Collection>,
+    ) -> tokio::task::JoinHandle<()> {
+        let coll_name = collection.watched.coll_name.clone();
+        tokio::spawn(async move {
+            loop {
+                match crate::mongo::connector::Connector::from_collection(
+                    &settings.mongo_uri,
+                    &settings.rabbitmq_uri,
+                    &collection,
+                )
+                .await
+                {
+                    Ok(connector) => {
+                        if let Err(e) = connector.connect(&collection.watched.coll_name).await {
+                            tracing::error!(error = ?e, collection = %coll_name, "Watcher failed, notifying parent");
+                        }
+                        let _ = tx.send(collection.clone());
+                        break;
+                    }
+                    Err(e) => {
+                        tracing::error!(error = ?e, collection = %coll_name, "Failed to create connector, retrying in 5s");
+                        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+                    }
+                }
+            }
+        })
+    }
+
     async fn serve(&self) -> Result<(), Error> {
         use tokio::sync::mpsc;
 
@@ -30,51 +61,15 @@ impl ConnectorServer {
         let collections = self.settings.collections.clone();
         let settings = self.settings.clone();
 
-        // Helper closure to spawn a resilient job for a collection
-        let spawn_job =
-            |settings: Settings,
-             collection: crate::config::Collection,
-             tx: mpsc::UnboundedSender<crate::config::Collection>| {
-                let coll_name = collection.watched.coll_name.clone();
-                tokio::spawn(async move {
-                    loop {
-                        match crate::mongo::connector::Connector::from_collection(
-                            &settings.mongo_uri,
-                            &settings.rabbitmq_uri,
-                            &collection,
-                        )
-                        .await
-                        {
-                            Ok(connector) => {
-                                if let Err(e) =
-                                    connector.connect(&collection.watched.coll_name).await
-                                {
-                                    error!(error = ?e, collection = %coll_name, "Watcher failed, notifying parent");
-                                }
-                                let _ = tx.send(collection.clone());
-                                break;
-                            }
-                            Err(e) => {
-                                // DD: If one of the components like RabbitMQ or MongoDB is down, we should retry
-                                // after a delay.
-                                error!(error = ?e, collection = %coll_name, "Failed to create connector, retrying in 5s");
-                                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-                            }
-                        }
-                    }
-                })
-            };
-
         // Spawn all jobs
         for collection in collections {
-            let _handle = spawn_job(settings.clone(), collection.clone(), tx.clone());
+            Self::spawn(settings.clone(), collection.clone(), tx.clone());
         }
 
         info!("Connector server started");
 
         while let Some(collection) = rx.recv().await {
-            let _h = spawn_job(self.settings.clone(), collection, tx.clone());
-            // handles.insert(coll_name, handle);
+            Self::spawn(self.settings.clone(), collection, tx.clone());
         }
         Ok(())
     }
