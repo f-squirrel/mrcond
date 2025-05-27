fd07ce8704f8a81ffec0b6dc6c14a5905f038da4
f-squirrel <dmitry.b.danilov@gmail.com>
2025-05-27 23:09:47 +0300

    test1

--- mongodb_rabbitmq_connectord/tests/e2e.rs
+++ mongodb_rabbitmq_connectord/tests/e2e.rs
@@ -1,8 +1,5 @@
-use config;
-use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::{Child, Command, Stdio};
-use std::thread::sleep;
 use std::time::Duration;
 
 use bson::Document;
@@ -12,19 +9,48 @@ use mongodb::{bson::doc, Client};
 use mongodb_rabbitmq_connector::config::{Collection, Connections, Settings};
 use serde::Deserialize;
 
-fn start_cluster() -> Child {
-    let cluster = Command::new("make")
-        .args(&["down", "run"])
-        .stdout(Stdio::null())
-        .spawn()
-        .expect("Failed to start cluster");
-
-    cluster
+struct Cluster {
+    cluster: Child,
+    running: bool,
 }
 
-fn stop_cluster(cluster: &mut Child) {
-    let _ = cluster.kill();
-    let _ = cluster.wait();
+impl Cluster {
+    fn start() -> Self {
+        let cluster = Command::new("make")
+            .args(&["-C", "../", "down", "run"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start cluster");
+        Self {
+            cluster,
+            running: true,
+        }
+    }
+
+    fn stop(&mut self) {
+        if !self.running {
+            return;
+        }
+
+        let _ = self.cluster.kill();
+        let _ = self.cluster.wait();
+
+        let _ = Command::new("make")
+            .args(&["-C", "../", "down"])
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Failed to start cluster")
+            .wait()
+            .unwrap();
+
+        self.running = false;
+    }
+}
+
+impl Drop for Cluster {
+    fn drop(&mut self) {
+        self.stop();
+    }
 }
 
 /// Producer for inserting documents into MongoDB.
@@ -38,6 +64,7 @@ pub struct Producer {
 impl Producer {
     /// Initialize a Producer from a config file and a mongo_uri override.
     pub async fn new(client: mongodb::Client, collection: &Collection) -> Self {
+        println!("Connecting to MongoDB at {}", collection.watched.db_name);
         let db = client.database(&collection.watched.db_name);
         let collection = db.collection::<Document>(collection.watched.coll_name.as_str());
         Self {
@@ -48,13 +75,18 @@ impl Producer {
     }
 
     pub async fn send_bulk(&self, docs: &[serde_json::Value]) -> mongodb::error::Result<()> {
+        println!("deleting");
         self.collection.delete_many(doc! {}).await?;
+        // println!("deleted");
         if !docs.is_empty() {
             let docs: Vec<_> = docs
                 .iter()
                 .map(|v| mongodb::bson::to_document(v).unwrap())
                 .collect();
             self.collection.insert_many(docs).await?;
+            println!("Inserted documents into MongoDB");
+        } else {
+            println!("No documents to insert");
         }
         Ok(())
     }
@@ -68,8 +100,27 @@ pub struct Consumer {
 
 impl Consumer {
     pub async fn new(rabbitmq_uri: &str, queue: &str) -> lapin::Result<Self> {
-        let conn =
-            lapin::Connection::connect(rabbitmq_uri, ConnectionProperties::default()).await?;
+        let mut counter = 0;
+
+        let conn = loop {
+            match lapin::Connection::connect(rabbitmq_uri, ConnectionProperties::default()).await {
+                Ok(conn) => {
+                    println!("Connected to RabbitMQ at {}", rabbitmq_uri);
+                    break conn;
+                }
+                Err(e) => {
+                    println!("Failed to connect to RabbitMQ: {}. Retrying...", e);
+                    tokio::time::sleep(Duration::from_secs(10)).await;
+                    counter += 1;
+                    if counter >= 10 {
+                        panic!("Failed to connect to RabbitMQ after 10 attempts");
+                    }
+                }
+            }
+        };
+
+        // let conn =
+        //     lapin::Connection::connect(rabbitmq_uri, ConnectionProperties::default()).await?;
         let channel = conn.create_channel().await?;
         channel
             .queue_declare(queue, QueueDeclareOptions::default(), FieldTable::default())
@@ -93,7 +144,7 @@ impl Consumer {
                 let v: serde_json::Value = serde_json::from_slice(&data.data).unwrap();
                 received.push(v);
             } else {
-                sleep(Duration::from_millis(200));
+                tokio::time::sleep(Duration::from_millis(200)).await;
                 tries += 1;
             }
         }
@@ -103,15 +154,23 @@ impl Consumer {
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
 async fn test_producer_consumer_init_and_join() {
+    let base = std::env::current_dir().unwrap();
+    println!("Current directory: {:?}", base);
+
+    let mut cluster = Cluster::start();
+    tokio::time::sleep(Duration::from_secs(30)).await;
+
     let config = config::Config::builder()
-        .add_source(config::File::with_name("data/simple/config.yaml"))
+        .add_source(config::File::with_name("tests/data/simple/config.yaml"))
         .build()
         .unwrap();
 
     let settings = config.try_deserialize::<Settings>().unwrap();
 
     let config = config::Config::builder()
-        .add_source(config::File::with_name("data/simple/connections.yaml"))
+        .add_source(config::File::with_name(
+            "tests/data/simple/connections.yaml",
+        ))
         .build()
         .unwrap();
     let connections = config.try_deserialize::<Connections>().unwrap();
@@ -121,9 +180,28 @@ async fn test_producer_consumer_init_and_join() {
         collections: settings.collections,
     };
 
-    let client = Client::with_uri_str(settings.connections.mongo_uri)
-        .await
-        .unwrap();
+    println!("Settings: {:?}", settings);
+
+    let mut counter = 0;
+    let client = loop {
+        match Client::with_uri_str(settings.connections.mongo_uri.clone()).await {
+            Ok(client) => {
+                println!("Connected to MongoDB at {}", settings.connections.mongo_uri);
+                break client;
+            }
+            Err(e) => {
+                if counter >= 10 {
+                    panic!("Failed to connect to MongoDB after 10 attempts: {}", e);
+                }
+                println!("Failed to connect to MongoDB: {}. Retrying...", e);
+                tokio::time::sleep(Duration::from_secs(10)).await;
+                counter += 1;
+            }
+        }
+    };
+    // let client = Client::with_uri_str(settings.connections.mongo_uri)
+    //     .await
+    //     .unwrap();
 
     let producer = Producer::new(client, &settings.collections[0]).await;
     let consumer = Consumer::new(
@@ -135,24 +213,35 @@ async fn test_producer_consumer_init_and_join() {
 
     // Read input documents from file
     let docs: Vec<serde_json::Value> = {
-        let file = fs::File::open("data/simple/input.json").unwrap();
+        let cwd = std::env::current_dir().unwrap();
+        println!("Current directory: {:?}", cwd);
+        let file = std::fs::File::open("tests/data/simple/input.json").unwrap();
         serde_json::from_reader(file).unwrap()
     };
     let expected = docs.len();
 
+    println!("Sending {} documents to MongoDB", docs.len());
+    let res = producer.send_bulk(&docs).await;
+    println!("Producer finished sending documents: {:?}", res);
     // Spawn producer and consumer tasks
-    let producer_task = tokio::spawn({
-        let producer = producer.clone();
-        let docs = docs.clone();
-        async move {
-            producer.send_bulk(&docs).await.unwrap();
-        }
-    });
-    let consumer_task = tokio::spawn(async move {
-        let received = consumer.receive_all(expected, 20).await;
-        assert_eq!(received, docs);
-    });
+    // let producer_task = tokio::spawn({
+    //     let producer = producer.clone();
+    //     let docs = docs.clone();
+    //     async move {
+    //         println!("Sending {} documents to MongoDB", docs.len());
+    //         let res = producer.send_bulk(&docs).await;
+    //         println!("Producer finished sending documents: {:?}", res);
+    //     }
+    // });
 
-    // Join both tasks
-    let _ = tokio::try_join!(producer_task, consumer_task).unwrap();
+    tokio::time::sleep(Duration::from_secs(100)).await; // Wait for producer to send documents
+
+    // let consumer_task = tokio::spawn(async move {
+    //     let received = consumer.receive_all(expected, 1000).await;
+    //     assert_eq!(received, docs);
+    // });
+
+    // // Join both tasks
+    // let _ = tokio::try_join!(producer_task, consumer_task).unwrap();
+    cluster.stop();
 }
